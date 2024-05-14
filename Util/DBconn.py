ae72b1f4079a3341c3fc220d90b98d247dbee841
tonikareddy2 <tonikareddy2@gmail.com>
2024-05-14 14:15:47 +0530

    PropertyUtil organized

--- Util/DBconn.py
+++ Util/DBconn.py
@@ -1,18 +1,5 @@
 import pyodbc
-
-
-class PropertyUtil:
-    @staticmethod
-    def get_property_string():
-        server_name = "DESKTOP-P8QAI2N\SQLEXPRESS"
-        database_name = "HexawareMovieDB"
-        conn_str = (
-            f"Driver={{SQL Server}};"
-            f"Server={server_name};"
-            f"Database={database_name};"
-            f"Trusted_Connection=yes;"
-        )
-        return conn_str
+from Util.DBPropertyUtil import PropertyUtil
 
 
 class DBconnection:
